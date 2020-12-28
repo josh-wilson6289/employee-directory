a3fcdbcf92d05ec5e0eac7db3eed95b6c80a711b
Josh Wilson <joshua.wilson6289@gmail.com>
2020-12-28 15:45:14 -0600

    added a dummy main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Container from "./components/Container";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
      <Container>
       <Header />
-      
+        <Main />
 
      </Container>
     </div>
